a35130168ba0e51405433001af46eb3e888cd8c8
pham <pham@apecsa-indonesia.com>
2017-11-05 16:59:55 +0900

    Fix problem column in schema not found

--- samplebatchprocessing/src/main/java/hpg/model/entity/NewHire.java
+++ samplebatchprocessing/src/main/java/hpg/model/entity/NewHire.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package hpg.model.entity;
 
 import java.io.Serializable;
@@ -23,6 +22,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  * Created on Oct 16, 2017
+ *
  * @author Y@techburg
  */
 @Entity
